ac61b9b858c3949e47ada6e80e3b9b118d326bdd
NachoMol <ignaciotmolina@gmail.com>
2024-10-08 19:17:17 -0300

    creando clase base para tests

---
+++ proyecto-testing/src/main/java/BasePage.java
@@ -0,0 +1,51 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BasePage {
+
+    public WebDriver driver;
+
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+
+
+    public void setup() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    public void url(String url){
+        driver.get(url);
+    }
+
+    public void close() {
+        driver.quit();
+    }
+
+
+    public WebElement findElement(By locator){
+        return driver.findElement(locator);
+    }
+
+    public void sendText(String inputText, By locator){
+        this.findElement(locator).clear();
+        this.findElement(locator).sendKeys(inputText);
+    }
+
+    public void sendKey(CharSequence key,  By locator){
+        this.findElement(locator).sendKeys(key);
+    }
+
+    public void click(By locator){
+        this.findElement(locator).click();
+    }
+
+    public String getText(By locator){
+        return this.findElement(locator).getText();
+    }
+    
+}
