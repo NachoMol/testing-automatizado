705992efd33249faed9e0f8558517d2882d07502
NachoMol <ignaciotmolina@gmail.com>
2024-10-22 19:57:27 -0300

    implementando reportes con Extent

---
+++ proyecto-testing/src/main/java/reportes/ExtentFactory.java
@@ -0,0 +1,16 @@
+package reportes;
+
+import com.aventstack.extentreports.ExtentReports;
+
+public class ExtentFactory {
+
+    public static ExtentReports getInstance(){
+        ExtentReports extent = new ExtentReports();
+        extent.setSystemInfo("OS","Windows");
+        extent.setSystemInfo("Navegador","Chrome");
+        extent.setSystemInfo("Ambiente","QA");
+        return extent;
+    }
+
+
+}
--- proyecto-testing/src/test/java/SearchTest.java
+++ proyecto-testing/src/test/java/SearchTest.java
@@ -1,15 +1,17 @@
 //Pagina utilizada: https://digital-booking-front.digitalhouse.com/
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import reportes.ExtentFactory;
 
 import java.time.Duration;
 
@@ -17,6 +19,15 @@ public class SearchTest {
 
     public WebDriver driver;
     public WebDriverWait wait;
+
+    static ExtentSparkReporter info = new ExtentSparkReporter("target/Reporte.html");
+    static ExtentReports extent;
+
+    @BeforeAll
+    public static void createReport(){
+        extent = ExtentFactory.getInstance();
+        extent.attachReporter(info);
+    }
 
     @BeforeEach
     public void preconditions() throws InterruptedException{
@@ -33,14 +44,18 @@ public class SearchTest {
      @Tag("BUSQUEDA")
      @Tag("SMOKE")
     public void BusquedaExitosa() throws InterruptedException {
-
+         ExtentTest test = extent.createTest("BusquedaExitosa en Uruguay");
+         test.log(Status.INFO, "Comienza nuestro test de busqueda");
          SearchPage searchPage = new SearchPage(driver, wait);
 
          searchPage.escribirBusqueda("Punta del este");
 
          searchPage.clickBuscar();
-
+         test.log(Status.PASS, "Se realizo la busqueda de Uruguay exitosamente");
          searchPage.resultadoBusqueda();
+
+         test.log(Status.PASS, "Se valida que la busqueda haya sido correcta");
+
     }
 
     @Test
@@ -48,22 +63,32 @@ public class SearchTest {
     @Tag("REGRESSION")
     public void BusquedaExitosaGrecia() throws InterruptedException {
 
+        ExtentTest test = extent.createTest("BusquedaExitosa en Grecia");
+        test.log(Status.INFO, "Comienza nuestro test de busqueda");
+
         SearchPage searchPage = new SearchPage(driver, wait);
 
         searchPage.escribirBusqueda("Paros");
 
         searchPage.clickBuscar();
+        test.log(Status.PASS, "Se realizo la busqueda de Grecia exitosamente");
 
 
         searchPage.resultadoBusqueda();
 
-
+        test.log(Status.PASS, "Se valida que la busqueda haya sido correcta");
     }
 
     @AfterEach
     public void close(){
         SearchPage searchPage = new SearchPage(driver, wait);
         searchPage.close();
+    }
+
+
+    @AfterAll
+    public static void saveReport(){
+        extent.flush();
     }
 
 }
